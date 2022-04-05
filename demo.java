8cba8b40cdc9f65d706c771cb331b62e5cf0e402
root <root@ip-172-31-84-1.ec2.internal>
2022-04-05 12:35:14 +0000

    main code

--- demo.java
+++ demo.java
@@ -6,3 +6,18 @@ branch code()
 	p.o print("java code");
 }
 
+main code
+void main()
+{
+         p.o print("java code");
+	 p.o print("java code");
+	 p.o print("java code");
+	 p.o print("java code");
+	 p.o print("java code");
+
+
+
+
+
+
+}
